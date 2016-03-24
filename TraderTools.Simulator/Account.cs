a1e3a077ae01a2bf7aeaf24ed6b404e80b59fadc
Paul Tan <pjxtan@gmail.com>
2016-03-23 22:14:47 -0400

    Throw exceptions for invalid arguments to Account(), Deposit(), Withdraw()

--- TraderTools.Simulator/Account.cs
+++ TraderTools.Simulator/Account.cs
@@ -1,4 +1,6 @@
-﻿namespace TraderTools.Simulator
+﻿using System;
+
+namespace TraderTools.Simulator
 {
     public class Account
     {
@@ -6,16 +8,20 @@
 
         public Account(decimal initialBalance)
         {
+            if (initialBalance < 0) throw new ArgumentException("Balance cannot be negative");
             Balance = initialBalance;
         }
 
         public void Deposit(decimal amount)
         {
+            if (amount < 0) throw new ArgumentException("Cannot deposit negative amount; use Withdraw() instead");
             Balance += amount;
         }
 
         public void Withdraw(decimal amount)
         {
+            if (amount < 0) throw new ArgumentException("Cannot withdraw negative amount; use Deposit() instead");
+            if (Balance - amount < 0) throw new ArgumentException("Cannot withdraw amount greater than balance");
             Balance -= amount;
         }
     }
